6d24ff60f4e7e86a767ea792adf62848c081c5f3
Ryan Locascio <loc.fitness1@gmail.com>
2020-04-30 18:42:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(num)
-  if (num % 5 == 0) && (num % 3 == 0)
-    return  "Fizzbuzz" 
+  if (num % 15 == 3) && (num % 15 == 5)
+    return  "Fizzbuzz"
   elsif num % 3 == 0
     return  "Fizz"
   elsif num % 5 == 0
