b0c4a8324634707650345eb0b3c96edf1fae7a0f
Ryan Locascio <loc.fitness1@gmail.com>
2020-04-30 18:14:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,12 @@
-def fizzbuzz
-
+def fizzbuzz(num)
+  if (num % 5 == 0) && (num % 3 == 0)
+    return = "Fizzbuzz"
+  elsif num % 3 == 0
+    return = "Fizz"
+  elsif num % 5 == 0
+    return = "Buzz"
+  else
+    return
+  end       
 end# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
